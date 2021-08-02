97c35a838083ea3d3e74e965cbeb31e6761eae72
Gera2019 <gumanoid2@gmail.com>
2021-08-02 12:14:20 +0300

    Update of task_3.py. Check of x1 and x2 equality was added.

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -7,13 +7,17 @@ k = 1/(x2 - x1)
 b = y1/(y2 - y1) - x1/(x2 - x1)
 '''
 
+
 def get_equation(x1, y1, x2, y2):
-    pnt1 = {'x': x1, 'y': y1}
-    pnt2 = {'x': x2, 'y': y2}
-    print(pnt1, pnt2)
-    k = 1 / (pnt2['x'] - pnt1['x'])
-    b = pnt1['y'] / (pnt2['y'] - pnt1['y']) - pnt1['x'] / (pnt2['x'] - pnt1['x'])
-    equation = f'y={k}x{+b:+}'
+    if x1 == x2:
+        equation = f'y={x1}'
+    else:
+        pnt1 = {'x': x1, 'y': y1}
+        pnt2 = {'x': x2, 'y': y2}
+        print(pnt1, pnt2)
+        k = 1 / (pnt2['x'] - pnt1['x'])
+        b = pnt1['y'] / (pnt2['y'] - pnt1['y']) - pnt1['x'] / (pnt2['x'] - pnt1['x'])
+        equation = f'y={k}x{+b:+}'
     return equation
 
 
